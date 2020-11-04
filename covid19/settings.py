65988837636b3bd1ff66d376e4fa7619287fe726
Mingfeng Yu <mfdevworld@gmail.com>
2020-11-04 11:07:07 +0200

    Update settings
    Add django.contrib.gis, rest_framework and rest_framework_gis to INSTALLED_APPS

--- covid19/settings.py
+++ covid19/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django built-in apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    # third-party apps
+    'rest_framework',
+    'rest_framework_gis',
+    # project apps
 ]
 
 MIDDLEWARE = [
